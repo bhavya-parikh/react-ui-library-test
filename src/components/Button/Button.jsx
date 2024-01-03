932c259e62a2f29c16764cc2059d70af7604f6cb
Bhavya-Parikh <12102080701016@mbit.edu.in>
2024-01-04 00:37:58 +0530

    added color prop

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import buttonCss from "./Button.module.css";
 function Button({ size, color, content }) {
-  const buttonClasses = `${buttonCss.button} ${buttonCss[size]}`;
+  const buttonClasses = `${buttonCss.button} ${buttonCss[size]} ${buttonCss[color]}`;
   return <button className={buttonClasses}>{content}</button>;
 }
 
